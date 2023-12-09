53c71f201dd9350339ed19c935ffc798b6626fc9
=Erick Rosario <=developeredra@gmail.com>
2023-12-08 22:28:21 -0400

    .Se conectó el inicio del dashboard y la tabla de asignación principal para la web está conectada.

--- src/app/pages/guest/header/header.component.ts
+++ src/app/pages/guest/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FilterToggleSidebarService } from 'src/app/services/filter-toggle-sidebar.service';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,14 @@ import { FilterToggleSidebarService } from 'src/app/services/filter-toggle-sideb
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  constructor(public filterMenuService: FilterToggleSidebarService) {}
+  constructor(public filterMenuService: FilterToggleSidebarService, private movieService: MovieService) {}
   imagePath: string = 'assets/logo_white_completed.png'
 
   openFilterMenu(): void {
     this.filterMenuService.toggleMenu();
+  }
+
+  showMovies(type: string): void {
+    // Lógica para filtrar la lista según el tipo de película
   }
 }
